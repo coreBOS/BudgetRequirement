7d9a0a404e27529857e3cdbe7b125dd46c5dd996
glorymoshi <glorysimon95@gmail.com>
2021-06-11 10:43:57 +0300

    feat(module)create module BudgetRequirement

---
+++ module/cbBudgetRequirement/language/en_us.lang.php
@@ -0,0 +1,33 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = array(
+	'cbBudgetRequirement' => 'Budget Requirements',
+	'SINGLE_cbBudgetRequirement' => 'Budget Requirement',
+	'cbBudgetRequirement ID' => 'BudReq ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_cbBudgetRequirement_INFORMATION' => 'Budget Requirement',
+	'LBL_DESCRIPTION_INFORMATION' => 'Reason',
+
+	'Budget Requirement No' => 'Budget Requirement No',
+	'Requirement' => 'Requirement',
+	'Income' => 'Income',
+	'Expense' => 'Expense',
+	'Requirement Status' => 'Requirement Status',
+	'Requirement Budget Allocation' => 'Requirement Budget Allocation',
+	'Purchase Order' => 'Purchase Order',
+	'Sales Order' => 'Sales Order',
+	'Project' => 'Project',
+	'Convert to PurchaseOrder' => 'Convert to PurchaseOrder',
+	'Convert to SalesOrder' => 'Convert to SalesOrder',
+	'Convert to Project' => 'Convert to Project',
+);
+?>
